b96bb5a63dd2bcc622725ac644f0f0018246876c
Jasper Van der Jeugt <m@jaspervdj.be>
2014-06-06 15:43:01 +0200

    Add lookup test

--- tests/Data/IntPSQ/Tests.hs
+++ tests/Data/IntPSQ/Tests.hs
@@ -2,7 +2,8 @@ module Data.IntPSQ.Tests
     ( tests
     ) where
 
-import           Prelude   hiding (null)
+import           Prelude             hiding (null, lookup)
+import           Control.Applicative ((<$>))
 
 import           Test.HUnit                           (Assertion, (@?=))
 import           Test.Framework                       (Test)
@@ -12,13 +13,15 @@ import           Data.IntPSQ
 
 tests :: [Test]
 tests =
-    [ testCase "size" test_size
-    , testCase "size2" test_size2
-    , testCase "empty" test_empty
+    [ testCase "size"   test_size
+    , testCase "size2"  test_size2
+    , testCase "empty"  test_empty
+    , testCase "lookup" test_lookup
     ]
 
 type UIntPSQ = IntPSQ Int ()
 type CIntPSQ = IntPSQ Int Char
+type IIntPSQ = IntPSQ Int Int
 
 test_size :: Assertion
 test_size = do
@@ -36,3 +39,18 @@ test_empty :: Assertion
 test_empty = do
     toList (empty :: UIntPSQ) @?= []
     size empty @?= 0
+
+test_lookup :: Assertion
+test_lookup = do
+    employeeCurrency 1 @?= Just 1
+    employeeCurrency 2 @?= Nothing
+  where
+    employeeDept    = fromList [(1, 100, 2), (3, 101, 1)] :: IIntPSQ
+    deptCountry     = fromList [(1, 102, 1), (2, 103, 2)] :: IIntPSQ
+    countryCurrency = fromList [(1, 104, 2), (2, 105, 1)] :: IIntPSQ
+
+    employeeCurrency :: Int -> Maybe Int
+    employeeCurrency name = do
+        dept    <- snd <$> lookup name employeeDept
+        country <- snd <$> lookup dept deptCountry
+        snd <$> lookup country countryCurrency
