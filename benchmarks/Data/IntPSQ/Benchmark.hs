f67bc8601e2eda4052a8c8a97727837d69b34ae2
Alex Sayers <alex.sayers@gmail.com>
2014-06-08 13:12:45 +0200

    Remove benchmarks for unexposed IntPSQ functions

--- benchmarks/Data/IntPSQ/Benchmark.hs
+++ benchmarks/Data/IntPSQ/Benchmark.hs
@@ -19,16 +19,6 @@ benchmark getElem benchmarkSize = bgroup "IntPSQ"
       , bench "insert (duplicates)" $ whnf (ins firstElems) initialPSQ
       -- , bench "insert (decreasing)" $ whnf (ins elemsDecreasing) initialPSQ
       -- , bench "fromList" $ whnf fromList elems_with_unit
-
-      , bench "insert2 (fresh)" $ whnf (ins2 firstElems) empty
-      , bench "insert2 (duplicates)" $ whnf (ins2 firstElems) initialPSQ
-      -- , bench "insert2 (decreasing)" $ whnf (ins2 elemsDecreasing) initialPSQ
-      , bench "fromList2" $ whnf fromList2 elems
-
-      , bench "insert3 (fresh)" $ whnf (ins3 firstElems) empty
-      , bench "insert3 (duplicates)" $ whnf (ins3 firstElems) initialPSQ
-      -- , bench "insert3 (decreasing)" $ whnf (ins3 elemsDecreasing) initialPSQ
-      , bench "fromList3" $ whnf fromList3 elems
       ]
   where
     (firstElems, secondElems) = splitAt (benchmarkSize `div` 2) elems
@@ -37,22 +27,11 @@ benchmark getElem benchmarkSize = bgroup "IntPSQ"
 
     initialPSQ = fromList firstElems :: IntPSQ Int ()
 
-
-
--- Benchmarking our IntPSQ type
--------------------------------------------------------------------------------
-
 lookup' :: [Int] -> IntPSQ Int () -> Int
 lookup' xs m = foldl' (\n k -> maybe n fst (lookup k m)) 0 xs
 
 ins :: [(Int, Int, ())] -> IntPSQ Int () -> IntPSQ Int ()
 ins xs m0 = foldl' (\m (k, p, v) -> insert k p v m) m0 xs
-
-ins2 :: [(Int, Int, ())] -> IntPSQ Int () -> IntPSQ Int ()
-ins2 xs m0 = foldl' (\m (k, p, v) -> insert2 k p v m) m0 xs
-
-ins3 :: [(Int, Int, ())] -> IntPSQ Int () -> IntPSQ Int ()
-ins3 xs m0 = foldl' (\m (k, p, v) -> insert3 k p v m) m0 xs
 
 deleteMins :: IntPSQ Int () -> Int
 deleteMins = go 0
